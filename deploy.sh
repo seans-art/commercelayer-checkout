482feb7ca584977c24625c6625a138b3325df063
alexstouffer <bizkite@hotmail.com>
2021-06-24 15:09:40 -0700

    deploy dist script mod

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,11 @@ set -e
 # build
 npm run build
 # navigate into the build output directory
+cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+git init
 git add -A
 git commit -m 'deploy'
-git push git@github.com:seans-art/commercelayer-checkout.git github-deploy
+git push -f git@github.com:seans-art/commercelayer-checkout.git dist-deploy
+cd -
