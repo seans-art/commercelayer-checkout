04c1d8dadd5f38886db89de087de5512ae93878c
alexstouffer <bizkite@hotmail.com>
2021-06-24 15:46:47 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
 npm run build
+
 # navigate into the build output directory
+cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
+git init
 git add -A
 git commit -m 'deploy'
-git push git@github.com:seans-art/commercelayer-checkout.git github-deploy
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:seans-art/commercelayer-checkout.git github-deploy:dist-deploy
+
+cd -
