bfb7e4b09c08bbc5a32fbfbca0d4b3e6bb963a9a
alexstouffer <bizkite@hotmail.com>
2021-06-24 14:59:01 -0700

    remove publicPath from vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/hello-world/'
-    : '/',
   devServer: {
     disableHostCheck: true,
     public: process.env.VUE_APP_DEV_SERVER,
